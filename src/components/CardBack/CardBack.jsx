e13f87c696a6a5436850c2223001a37f853d6a52
wowermalwina <72358702+wowermalwina@users.noreply.github.com>
2021-01-10 14:57:50 +0000

    changes added to card

--- src/components/CardBack/CardBack.jsx
+++ src/components/CardBack/CardBack.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import styles from "./CardBack.module.scss";
 
 const CardBack = (props) => {
-  const { description, ingredients, food_pairing, ebc, ibu, ph} = props.beer
+  const { description, food_pairing, ebc, ibu, ph} = props.beer
+
+    const food = food_pairing.map((food, index) => {
+
+    return (
+      <li key={index}>
+        {food}
+      </li>
+    )
+  });
   
   return (
     <section className={styles.cardBack}>
       <h1>Description:</h1>
       <p>{description}</p>
-      <p>{description}</p>
-      {/* <p>{ingredients}</p> */}
-      <p>{food_pairing}</p>
+      <h1>Food Pairings:</h1>
+      <p>{food}</p>
       <p>{ebc}</p>
       <p>{ibu}</p>
       <p>{ph}</p>
